4dad5bc1525608ee0ad6e28c777e35be0c6dddcf
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-06-18 12:20:32 -0400

    first couple lectures, creating form,labels, inputs, and card styles with css module styling

---
+++ 01-starting-project/src/components/Users/AddUser.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <lable htmlFor="username">Usernamer</lable>
+        <input id="username" type="text" />
+        <lable htmlFor="age">Age (Years)</lable>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
